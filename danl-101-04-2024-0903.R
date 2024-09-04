00d309d7f6256a995a7c4b384e0d42a2f05b391b
bcdanl <bchoe@geneseo.edu>
2024-09-04 01:24:35 +0000

    code

---
+++ danl-101-04-2024-0903.R
@@ -0,0 +1,12 @@
+# install.packages("tidyverse")
+# make a comment on your R code
+#   or the result of your code
+#   or any idea
+
+library(tidyverse)
+
+ggplot(data = mpg) +
+  geom_histogram(mapping = aes(x = hwy))
+
+mpg
+
